885c359b00046eb88685fe92ec6817e2bd769dce
Hoang Phan <hpha0005@student.monash.edu>
2021-09-11 08:04:46 +1000

    Providing option to hide a components with useState hook

--- src/components/extended/AppBox.js
+++ src/components/extended/AppBox.js
@@ -1,14 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import AdaptableContext from '../../utils/adaptableContext';
 import { AdaptableBox } from '../adaptable/AdaptableBox';
 import { AdaptableTypography } from '../adaptable/AdaptableTypography';
+import IconButton from '@material-ui/core/IconButton';
+import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        marginTop: "5px",
+        display: "inline-flex",
+    },
+}))
 
 export default function AppBox() {
+    const classes = useStyles()
     const { darkMode } = useContext(AdaptableContext)
+    const [show, setShow] = useState(true);
 
     return (
-        <AdaptableBox darkMode={darkMode}>
-            <AdaptableTypography darkMode={darkMode} variant="h1"> Hello </AdaptableTypography>
+        <AdaptableBox className={classes.root} darkMode={darkMode}>
+            <IconButton onClick={() => setShow(prev => !prev)}>
+                {show ? <IndeterminateCheckBoxIcon fontSize="small" /> : <AddBoxIcon fontSize="small" />}
+            </IconButton>
+            {show &&
+                <AdaptableTypography className={classes.inline} darkMode={darkMode} variant="h1">
+                    Hello
+                </AdaptableTypography>
+            }
         </AdaptableBox>
     );
 }
