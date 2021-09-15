15059dd13c3a8111f34efdd02ae83d56a3452b7a
Hoang Phan <hpha0005@student.monash.edu>
2021-09-15 17:51:28 +1000

    Moved show/hide button from app level to component level to allow reusability

--- src/components/extended/AppBox.js
+++ src/components/extended/AppBox.js
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AdaptableBox from '../adaptable/AdaptableBox';
 import AdaptableTypography from '../adaptable/AdaptableTypography';
-import IconButton from '@material-ui/core/IconButton';
-import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
-import AddBoxIcon from '@material-ui/icons/AddBox';
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
@@ -15,18 +12,12 @@ const useStyles = makeStyles(() => ({
 
 export default function AppBox() {
     const classes = useStyles()
-    const [show, setShow] = useState(true);
 
     return (
-        <AdaptableBox className={classes.root}>
-            <IconButton onClick={() => setShow(prev => !prev)}>
-                {show ? <IndeterminateCheckBoxIcon fontSize="small" /> : <AddBoxIcon fontSize="small" />}
-            </IconButton>
-            {show &&
-                <AdaptableTypography variant="h1">
-                    Hello
-                </AdaptableTypography>
-            }
+        <AdaptableBox className={classes.root} optional>
+            <AdaptableTypography variant="h1">
+                Hello
+            </AdaptableTypography>
         </AdaptableBox>
     );
 }
